f507f7c9094e074b6f590ffd13ff6c8640baadfb
Justin Mclean <justin@datastrato.com>
2024-05-20 12:58:47 +1000

    Only go back 6 months to help with large repos. All add option to cache read issues/PRs.

--- cache_github.py
+++ cache_github.py
@@ -1,14 +1,20 @@
 #
-# Cache GitHub PR,issues and comment from a GitHub repository.
+# Cache GitHub PR, issues and comments from a GitHub repository.
 #
 from github import Github
 
-import githubSetup
-
+import datetime
 import pickle
 
+import githubSetup
+
 save_every = 20 # Save to disk evey X PR or issues cached
+skip_cached = False # Skip re-reding any issues and PRs
 skip_closed = True # Skip re-reding closed issues and PRs
+
+# Don't go back further than this (default six months)
+past = datetime.date.today()
+past -= datetime.timedelta(6 * 30)
 
 def write_prs(prs, comments_pr, merged):
     print("Writing PR files")
@@ -33,27 +39,31 @@ def cache_prs(newprs, oldprs, oldcomments, oldmerged):
     i = 0
 
     for pr in newprs:
-        login = pr.user.login
-        no = pr.number
-        if skip_closed and no in oldprs and oldprs[no].closed_at and pr.closed_at:
-            print(f"{no} by {login} skipped")
-            prs[no] = oldprs[no]
-            comments_pr[no] = oldcomments[no]
-            if no in oldmerged:
-                merged[no] = oldmerged[no]
+        dt = pr.created_at.date()
+        if dt > past:
+            login = pr.user.login
+            no = pr.number
+            if skip_cached and no in oldprs or skip_closed and no in oldprs and oldprs[no].closed_at and pr.closed_at:
+                print(f"{no} by {login} skipped")
+                prs[no] = oldprs[no]
+                comments_pr[no] = oldcomments[no]
+                if no in oldmerged:
+                    merged[no] = oldmerged[no]
+                else:
+                    merged[no] = pr.is_merged()
             else:
+                print(f"{no} by {login} updated")
                 merged[no] = pr.is_merged()
+                prs[no] = pr
+                all_comments = pr.get_comments()
+                array = []
+                for commment in all_comments:
+                    array.append(commment)
+                comments_pr[no] = array
+                if i % save_every == 0:
+                    write_prs(prs, comments_pr, merged)
         else:
-            print(f"{no} by {login} updated")
-            merged[no] = pr.is_merged()
-            prs[no] = pr
-            all_comments = pr.get_comments()
-            array = []
-            for commment in all_comments:
-                array.append(commment)
-            comments_pr[no] = array
-            if i % save_every == 0:
-                write_prs(prs, comments_pr, merged)
+            break
         i = i + 1
 
     write_prs(prs, comments_pr, merged)
@@ -64,23 +74,27 @@ def cache_issues(newissues, oldissues, oldcomments):
     i = 0
 
     for issue in newissues:
-        login = issue.user.login
-        no = issue.number
-        if skip_closed and no in oldissues and oldissues[no].closed_at and issue.closed_at:
-            print(f"{no} by {login} skipped")
-            issues[no] = oldissues[no]
-            comments_issues[no] = oldcomments[no]
+        dt = issue.created_at.date()
+        if dt > past:
+            login = issue.user.login
+            no = issue.number
+            if skip_cached and no in oldissues or skip_closed and no in oldissues and oldissues[no].closed_at and issue.closed_at:
+                print(f"{no} by {login} skipped")
+                issues[no] = oldissues[no]
+                comments_issues[no] = oldcomments[no]
+            else:
+                print(f"{no} by {login} updated")
+                issues[no] = issue
+                all_comments = issue.get_comments()
+                array = []
+                for commment in all_comments:
+                    array.append(commment)
+                comments_issues[no] = array
+                # Save as we go in case something goes wrong
+                if i % save_every == 0:
+                    write_issues(issues, comments_issues)
         else:
-            print(f"{no} by {login} updated")
-            issues[no] = issue
-            all_comments = issue.get_comments()
-            array = []
-            for commment in all_comments:
-                array.append(commment)
-            comments_issues[no] = array
-            # Save as we go in case something goes wrong
-            if i % save_every == 0:
-                write_issues(issues, comments_issues)
+            break
         i = i + 1
 
     write_issues(issues, comments_issues)
@@ -94,10 +108,8 @@ def cache_gitgub():
     print(f"Open issues: {repo.open_issues_count}")
     print(f"Stars: {repo.stargazers_count}")
 
-    newprs = repo.get_pulls(state='all', base=githubSetup.branch)
-    print(f"PRs: {newprs.totalCount}")
+    newprs = repo.get_pulls(state='all', base=githubSetup.branch) # defaults to sorting by decending created date
     newissues = repo.get_issues(state='all')
-    print(f"Issues: {newissues.totalCount}")
 
     print("Reading PRs")
     with open('prs.pickle', 'rb') as pickle_file:
@@ -107,7 +119,6 @@ def cache_gitgub():
     with open('merged.pickle', 'rb') as pickle_file:
         oldmerged = pickle.load(pickle_file)
 
-    print(newprs.totalCount)
     cache_prs(newprs, oldprs, oldcomments, oldmerged)
 
     print("Reading Issues")
